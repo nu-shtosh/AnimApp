90a6d04cbd65410b93baa995ac2001f1ec578602
ILIA <dubenskiiilia@gmail.com>
2022-11-02 20:54:31 +0100

    some marks

--- AnimApp/Services/DataStore.swift
+++ AnimApp/Services/DataStore.swift
@@ -10,67 +10,8 @@ class DataStore {
 
     static let shared = DataStore()
 
-    var animations: [AnimationPreset] = [
-        .pop,
-        .slideLeft,
-        .slideRight,
-        .slideDown,
-        .slideUp,
-        .squeezeLeft,
-        .squeezeRight,
-        .squeezeDown,
-        .squeezeUp,
-        .fadeIn,
-        .fadeOut,
-        .fadeOutIn,
-        .fadeInLeft,
-        .fadeInRight,
-        .fadeInDown,
-        .fadeInUp,
-        .zoomIn,
-        .zoomOut,
-        .fall,
-        .shake,
-        .flipX,
-        .flipY,
-        .morph,
-        .squeeze,
-        .flash,
-        .wobble,
-        .swing,
-    ]
-
-    let curves: [AnimationCurve] = [
-        .easeIn,
-        .easeOut,
-        .easeInOut,
-        .linear,
-        .spring,
-        .easeInSine,
-        .easeOutSine,
-        .easeInOutSine,
-        .easeInQuad,
-        .easeOutQuad,
-        .easeInOutQuad,
-        .easeInCubic,
-        .easeOutCubic,
-        .easeInOutCubic,
-        .easeInQuart,
-        .easeOutQuart,
-        .easeInOutQuart,
-        .easeInQuint,
-        .easeOutQuint,
-        .easeInOutQuint,
-        .easeInExpo,
-        .easeOutExpo,
-        .easeInOutExpo,
-        .easeInCirc,
-        .easeOutCirc,
-        .easeInOutCirc,
-        .easeInBack,
-        .easeOutBack,
-        .easeInOutBack,
-    ]
+    var animations = AnimationPreset.allCases
+    let curves = AnimationCurve.allCases
 
     init() {}
 }
--- AnimApp/ViewControllers/ViewController.swift
+++ AnimApp/ViewControllers/ViewController.swift
@@ -19,16 +19,17 @@ class ViewController: UIViewController {
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var delayLabel: UILabel!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLabels()
+    }
+
     // MARK: - Private properties
     private var randomAnimation = Animation.getRandomAnimation()
 
     // MARK: - IBActions
     @IBAction func runButtonDidTapped(_ sender: UIButton) {
-        presetLabel.text = "Preset: \(randomAnimation.name)"
-        curveLabel.text = "Curve: \(randomAnimation.curve)"
-        forceLabel.text = "Force: \(String(format: "%.2f", randomAnimation.force))"
-        durationLabel.text = "Duration: \(String(format: "%.2f", randomAnimation.duration))"
-        delayLabel.text = "Delay: \(String(format: "%.2f", randomAnimation.delay))"
+        setLabels()
 
         animationView.animation = randomAnimation.name
         animationView.curve = randomAnimation.curve
@@ -42,3 +43,13 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - Private Methods
+extension ViewController {
+    private func setLabels() {
+        presetLabel.text = "Preset: \(randomAnimation.name)"
+        curveLabel.text = "Curve: \(randomAnimation.curve)"
+        forceLabel.text = "Force: \(String(format: "%.2f", randomAnimation.force))"
+        durationLabel.text = "Duration: \(String(format: "%.2f", randomAnimation.duration))"
+        delayLabel.text = "Delay: \(String(format: "%.2f", randomAnimation.delay))"
+    }
+}
